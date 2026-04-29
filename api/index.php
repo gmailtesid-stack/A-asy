ce6a876093d22a9b51d561f47f6d2d68fb891b45
readmialhitam-commits <readmialhitam@gmail.com>
2026-04-29 21:07:02 +0700

    Allow standard Laravel exceptions to pass through in api/index.php to enable normal redirects

--- api/index.php
+++ api/index.php
@@ -60,6 +60,13 @@ $response->send();
     $kernel->terminate($request, $response);
 
 } catch (\Throwable $e) {
+    // Biarkan exception Laravel (seperti Belum Login atau Validasi) ditangani secara normal
+    if ($e instanceof \Illuminate\Auth\AuthenticationException || 
+        $e instanceof \Illuminate\Validation\ValidationException ||
+        $e instanceof \Symfony\Component\HttpKernel\Exception\HttpException) {
+        throw $e;
+    }
+
     header('Content-Type: text/html', true, 500);
     echo "<div style='font-family:sans-serif; padding:20px; border:5px solid red; background:#fff1f1;'>";
     echo "<h1>🚨 LOGIN ERROR DIAGNOSIS</h1>";
